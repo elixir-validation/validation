5a9a14056a8b4c6f0d0e9afcf5af115bfe3ebb20
Matheus Gontijo <matheusla2@gmail.com>
2019-09-10 09:46:31 -0300

    updated documentation and validation wrappers

--- lib/validation.ex
+++ lib/validation.ex
@@ -17,18 +17,18 @@ defmodule Validation do
   Validates whether the input is alpha.
 
       # {:ok}
-      V.validate("")
-      V.validate("a")
-      V.validate("john")
-      V.validate("doe")
-      V.validate("foobar")
+      V.alpha("")
+      V.alpha("a")
+      V.alpha("john")
+      V.alpha("doe")
+      V.alpha("foobar")
 
       # {:error, _message}
-      V.validate("123")
-      V.validate("number 100%")
-      V.validate("@#$")
-      V.validate("_")
-      V.validate("dgç")
+      V.alpha("123")
+      V.alpha("number 100%")
+      V.alpha("@#$")
+      V.alpha("_")
+      V.alpha("dgç")
   """
   @spec alpha(String.t) :: Validation.default
   def alpha(input) when is_binary(input) do
@@ -39,14 +39,14 @@ defmodule Validation do
   Validates whether the input is alpha, excluding specific characters.
 
       # {:ok}
-      V.validate("john_", "_")
-      V.validate("google.com", ".")
-      V.validate("john doe", " ")
+      V.alpha("john_", "_")
+      V.alpha("google.com", ".")
+      V.alpha("john doe", " ")
 
       # {:error, _message}
-      V.validate("john_123", "$")
-      V.validate("google.com321", "*")
-      V.validate("john doe1", "_")
+      V.alpha("john_123", "$")
+      V.alpha("google.com321", "*")
+      V.alpha("john doe1", "_")
   """
   @spec alpha(String.t, String.t) :: Validation.default
   def alpha(input, excluded_characters) when is_binary(input) and is_binary(excluded_characters) do
@@ -89,12 +89,12 @@ defmodule Validation do
   Validates ranges.
 
       # {:ok}
-      V.validate(15, 10, 20)
-      V.validate(70, 0, 100)
+      V.between(15, 10, 20)
+      V.between(70, 0, 100)
 
       # {:error, _message}
-      V.validate(15, 20, 30)
-      V.validate(70, 300, 999)
+      V.between(15, 20, 30)
+      V.between(70, 300, 999)
   """
   @spec between(number, number, number) :: Validation.default
   def between(value, min, max) when is_number(value) and is_number(min) and is_number(max) do
@@ -105,18 +105,18 @@ defmodule Validation do
   Validates whether the input is a valid CNH (brazilian driver license ID).
 
       # {:ok}
-      V.validate("02650306461")
-      V.validate("04397322870")
-      V.validate("04375701302")
-      V.validate("02996843266")
-      V.validate("04375700501")
+      V.cnh("02650306461")
+      V.cnh("04397322870")
+      V.cnh("04375701302")
+      V.cnh("02996843266")
+      V.cnh("04375700501")
 
       # {:error, _message}
-      V.validate("")
-      V.validate("0000000000")
-      V.validate("9999999999")
-      V.validate("0265131640")
-      V.validate("0439732280")
+      V.cnh("")
+      V.cnh("0000000000")
+      V.cnh("9999999999")
+      V.cnh("0265131640")
+      V.cnh("0439732280")
   """
   @spec cnh(String.t) :: Validation.default
   def cnh(input) when is_binary(input) do
@@ -127,17 +127,17 @@ defmodule Validation do
   Validates whether the input is a valid CNPJ (brazilian company ID).
 
       # {:ok}
-      V.validate("32.063.364/0001-07")
-      V.validate("27.355.204/0001-00")
-      V.validate("36.310.327/0001-07")
-      V.validate("37550610000179")
-      V.validate("12774546000189")
+      V.cnpj("32.063.364/0001-07")
+      V.cnpj("27.355.204/0001-00")
+      V.cnpj("36.310.327/0001-07")
+      V.cnpj("37550610000179")
+      V.cnpj("12774546000189")
 
       # {:error, _message}
-      V.validate("12.345.678/9012-34")
-      V.validate("11.111.111/1111-11")
-      V.validate("00000000000000")
-      V.validate("99-010-0.")
+      V.cnpj("12.345.678/9012-34")
+      V.cnpj("11.111.111/1111-11")
+      V.cnpj("00000000000000")
+      V.cnpj("99-010-0.")
   """
   @spec cnpj(String.t) :: Validation.default
   def cnpj(input) when is_binary(input) do
@@ -148,20 +148,20 @@ defmodule Validation do
   Validates whether the input is a valid CPF (brazilian ID).
 
       # {:ok}
-      V.validate("350.45261819")
-      V.validate("693-319-118-40")
-      V.validate("11598647644")
-      V.validate("86734718697")
-      V.validate("3.6.8.8.9.2.5.5.4.8.8")
+      V.cpf("350.45261819")
+      V.cpf("693-319-118-40")
+      V.cpf("11598647644")
+      V.cpf("86734718697")
+      V.cpf("3.6.8.8.9.2.5.5.4.8.8")
 
       # {:error, _message}
-      V.validate("")
-      V.validate("01234567890")
-      V.validate("000.000.000-00")
-      V.validate("111.222.444-05")
-      V.validate("999999999.99")
-      V.validate("8.8.8.8.8.8.8.8.8.8.8")
-      V.validate("693-319-110-40")
+      V.cpf("")
+      V.cpf("01234567890")
+      V.cpf("000.000.000-00")
+      V.cpf("111.222.444-05")
+      V.cpf("999999999.99")
+      V.cpf("8.8.8.8.8.8.8.8.8.8.8")
+      V.cpf("693-319-110-40")
   """
   @spec cpf(String.t) :: Validation.default
   def cpf(input) when is_binary(input) do
@@ -172,16 +172,16 @@ defmodule Validation do
   Validates whether the input is has only consonants.
 
       # {:ok}
-      V.validate("w")
-      V.validate("y")
-      V.validate("qrst")
-      V.validate("bcdfghklmnp")
+      V.consonant("w")
+      V.consonant("y")
+      V.consonant("qrst")
+      V.consonant("bcdfghklmnp")
 
       # {:error, _message}
-      V.validate("a")
-      V.validate("ul")
-      V.validate("aeiou")
-      V.validate("Foo")
+      V.consonant("a")
+      V.consonant("ul")
+      V.consonant("aeiou")
+      V.consonant("Foo")
   """
   @spec consonant(String.t) :: Validation.default
   def consonant(input) when is_binary(input) do
@@ -192,13 +192,13 @@ defmodule Validation do
   Validates whether the input is has only consonants, excluding specific characters.
 
       # {:ok}
-      V.validate("www%", "%")
-      V.validate("bcd_fgh", "_")
-      V.validate("www www__www", " _")
+      V.consonant("www%", "%")
+      V.consonant("bcd_fgh", "_")
+      V.consonant("www www__www", " _")
 
       # {:error, _message}
-      V.validate("awww%", "%")
-      V.validate("wwwq", "_")
+      V.consonant("awww%", "%")
+      V.consonant("wwwq", "_")
   """
   @spec consonant(String.t, String.t) :: Validation.default
   def consonant(input, excluded_characters) when is_binary(input) and is_binary(excluded_characters) do
@@ -209,17 +209,17 @@ defmodule Validation do
   Validates whether the input is even.
 
       # {:ok}
-      V.validate(0)
-      V.validate(2)
-      V.validate(4)
-      V.validate(-2)
-      V.validate(9999998)
+      V.even(0)
+      V.even(2)
+      V.even(4)
+      V.even(-2)
+      V.even(9999998)
 
       # {:error, _message}
-      V.validate(1)
-      V.validate(5)
-      V.validate(-3)
-      V.validate(9999999)
+      V.even(1)
+      V.even(5)
+      V.even(-3)
+      V.even(9999999)
   """
   @spec even(Integer.t) :: Validation.default
   def even(input) when is_integer(input) do
@@ -230,17 +230,17 @@ defmodule Validation do
   Validates whether the input is lowercase.
 
       # {:ok}
-      V.validate("")
-      V.validate("lowercase")
-      V.validate("lowercase-with-dashes")
-      V.validate("lowercase with spaces")
-      V.validate("lowercase with specials characters like ã ç ê")
+      V.lowercase("")
+      V.lowercase("lowercase")
+      V.lowercase("lowercase-with-dashes")
+      V.lowercase("lowercase with spaces")
+      V.lowercase("lowercase with specials characters like ã ç ê")
 
       # {:error, _message}
-      V.validate("UPPERCASE")
-      V.validate("CamelCase")
-      V.validate("First Character Uppercase")
-      V.validate("With Numbers 1 2 3")
+      V.lowercase("UPPERCASE")
+      V.lowercase("CamelCase")
+      V.lowercase("First Character Uppercase")
+      V.lowercase("With Numbers 1 2 3")
   """
   @spec lowercase(String.t) :: Validation.default
   def lowercase(input) when is_binary(input) do
@@ -251,13 +251,13 @@ defmodule Validation do
   Validates whether the input is a valid MAC address.
 
       # {:ok}
-      V.validate("00:11:22:33:44:55")
-      V.validate("66-77-88-99-aa-bb")
-      V.validate("AF:0F:bd:12:44:ba")
+      V.mac_address("00:11:22:33:44:55")
+      V.mac_address("66-77-88-99-aa-bb")
+      V.mac_address("AF:0F:bd:12:44:ba")
 
       # {:error, _message}
-      V.validate("")
-      V.validate("00-1122:33:44:55")
+      V.mac_address("")
+      V.mac_address("00-1122:33:44:55")
   """
   @spec mac_address(String.t) :: default
   def mac_address(input) when is_binary(input) do
@@ -268,20 +268,20 @@ defmodule Validation do
   Validates if value is considered as "No".
 
       # {:ok}
-      V.validate("N")
-      V.validate("n")
-      V.validate("Nay")
-      V.validate("No")
-      V.validate("Nope")
-      V.validate("Not")
+      V.no("N")
+      V.no("n")
+      V.no("Nay")
+      V.no("No")
+      V.no("Nope")
+      V.no("Not")
 
       # {:error, _message}
-      V.validate("Donnot")
-      V.validate("Never")
-      V.validate("Niet")
-      V.validate("Noooooooo")
-      V.validate("Não")
-      V.validate("não")
+      V.no("Donnot")
+      V.no("Never")
+      V.no("Niet")
+      V.no("Noooooooo")
+      V.no("Não")
+      V.no("não")
   """
   @spec no(String.t) :: Validation.default
   def no(input) when is_binary(input) do
@@ -292,16 +292,16 @@ defmodule Validation do
   Validates whether the input is odd.
 
       # {:ok}
-      V.validate(1)
-      V.validate(5)
-      V.validate(-11)
-      V.validate(9999999)
+      V.odd(1)
+      V.odd(5)
+      V.odd(-11)
+      V.odd(9999999)
 
       # {:error, _message}
-      V.validate(0)
-      V.validate(8)
-      V.validate(-100)
-      V.validate(9999998)
+      V.odd(0)
+      V.odd(8)
+      V.odd(-100)
+      V.odd(9999998)
   """
   @spec odd(Integer.t) :: Validation.default
   def odd(input) when is_integer(input) do
@@ -312,17 +312,17 @@ defmodule Validation do
   Validates whether the input is uppercase.
 
       # {:ok}
-      V.validate("")
-      V.validate("UPPERCASE")
-      V.validate("UPPERCASE-WITH-DASHES")
-      V.validate("UPPERCASE WITH SPACES")
-      V.validate("UPPERCASE WITH NUMBERS 123")
+      V.uppercase("")
+      V.uppercase("UPPERCASE")
+      V.uppercase("UPPERCASE-WITH-DASHES")
+      V.uppercase("UPPERCASE WITH SPACES")
+      V.uppercase("UPPERCASE WITH NUMBERS 123")
 
       # {:error, _message}
-      V.validate("lowercase")
-      V.validate("CamelCase")
-      V.validate("First Character Uppercase")
-      V.validate("With Numbers 1 2 3")
+      V.uppercase("lowercase")
+      V.uppercase("CamelCase")
+      V.uppercase("First Character Uppercase")
+      V.uppercase("With Numbers 1 2 3")
   """
   @spec uppercase(String.t) :: Validation.default
   def uppercase(input) when is_binary(input) do
@@ -333,16 +333,16 @@ defmodule Validation do
   Validates whether the input is has only vowels.
 
       # {:ok}
-      V.validate("a")
-      V.validate("o")
-      V.validate("u")
-      V.validate("aeiou")
+      V.vowel("a")
+      V.vowel("o")
+      V.vowel("u")
+      V.vowel("aeiou")
 
       # {:error, _message}
-      V.validate("b")
-      V.validate("ul")
-      V.validate("16")
-      V.validate("\r")
+      V.vowel("b")
+      V.vowel("ul")
+      V.vowel("16")
+      V.vowel("\r")
   """
   @spec vowel(String.t) :: Validation.default
   def vowel(input) when is_binary(input) do
@@ -353,12 +353,12 @@ defmodule Validation do
   Validates whether the input is has only vowels.
 
       # {:ok}
-      V.validate("aaaa==", "=")
-      V.validate("aeoiu!!--", "!-")
+      V.vowel("aaaa==", "=")
+      V.vowel("aeoiu!!--", "!-")
 
       # {:error, _message}
-      V.validate("b==aaaa", "=")
-      V.validate("bc==aeoiu", "b!-")
+      V.vowel("b==aaaa", "=")
+      V.vowel("bc==aeoiu", "b!-")
   """
   @spec vowel(String.t, String.t) :: Validation.default
   def vowel(input, excluded_characters) when is_binary(input) and is_binary(excluded_characters) do
@@ -369,20 +369,20 @@ defmodule Validation do
   Validates if value is considered as "Yes".
 
       # {:ok}
-      V.validate("Y")
-      V.validate("Yea")
-      V.validate("Yeah")
-      V.validate("Yep")
-      V.validate("Yes")
-      V.validate("yes")
+      V.yes("Y")
+      V.yes("Yea")
+      V.yes("Yeah")
+      V.yes("Yep")
+      V.yes("Yes")
+      V.yes("yes")
 
       # {:error, _message}
-      V.validate("let's thrown an error")
-      V.validate("Yydoesnotmatter")
-      V.validate("Si")
-      V.validate("Sim")
-      V.validate("Yoo")
-      V.validate("Yiip")
+      V.yes("let's thrown an error")
+      V.yes("Yydoesnotmatter")
+      V.yes("Si")
+      V.yes("Sim")
+      V.yes("Yoo")
+      V.yes("Yiip")
   """
   @spec yes(String.t) :: Validation.default
   def yes(input) when is_binary(input) do
