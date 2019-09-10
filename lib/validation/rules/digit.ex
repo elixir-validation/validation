f57a95b92afdbc60a72062deb7710de172a723a7
Matheus Gontijo <matheusla2@gmail.com>
2019-09-10 15:01:22 -0300

    added digit rules

---
+++ lib/validation/rules/digit.ex
@@ -0,0 +1,24 @@
+defmodule Validation.Rules.Digit do
+  @moduledoc false
+
+  @spec validate(String.t) :: Validation.default
+  def validate(input) when is_binary(input) do
+    if String.match?(input, ~r/^[0-9]+$/) do
+      {:ok}
+    else
+      {:error, "Invalid digit value."}
+    end
+  end
+
+  @spec validate(String.t, String.t) :: Validation.default
+  def validate(input, excluded_characters) when is_binary(input) and is_binary(excluded_characters) do
+    excluded_characters_list = String.codepoints(excluded_characters)
+
+    {_, input} = Enum.map_reduce(excluded_characters_list, input, fn excluded_character, new_input ->
+      new_input = String.replace(new_input, excluded_character, "")
+      {excluded_character, new_input}
+    end)
+
+    validate(input)
+  end
+end
